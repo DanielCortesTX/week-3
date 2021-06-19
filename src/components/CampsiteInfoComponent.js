f0cfe5c7ac14a40d340a697d872964809e907b94
unknown <dcorteswebdev@gmail.com>
2021-06-19 12:45:15 -0500

    feat: Added that one thing

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -88,7 +88,7 @@ class CampsiteInfoComponent extends Component {
 					{comments.map(comment => {
 						return (
 							<div>
-								<p>{comment.text}</p>
+								<p>{comment.text}!!!!!</p>
 								<p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
 							</div>
 						)					
